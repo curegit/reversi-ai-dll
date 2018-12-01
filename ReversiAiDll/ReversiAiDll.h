26dae5391f77f196732091cb1c54aa4d29fda397
curegit <37978051+curegit@users.noreply.github.com>
2018-12-01 14:23:48 +0900

    ビット番号と座標の変換

--- ReversiAiDll/ReversiAiDll.h
+++ ReversiAiDll/ReversiAiDll.h
@@ -3,12 +3,21 @@
 // 外部にエクスポートされる関数を定義します
 extern "C"
 {
-	// 
-	__declspec(dllexport) int encode_position(int i, int j);
+	// 定数黒を返す
+	__declspec(dllexport) int const_dark();
 
-	// 
-	__declspec(dllexport) int decode_position_i(int position);
+	// 定数白を返す
+	__declspec(dllexport) int const_light();
 
-	// 
-	__declspec(dllexport) int decode_position_j(int position);
+	// 座標位置からビット番号を返す
+	__declspec(dllexport) int position_to_index(int i, int j);
+
+	// ビット番号からi座標を返す
+	__declspec(dllexport) int index_to_position_i(int n);
+
+	// ビット番号からj座標を返す
+	__declspec(dllexport) int index_to_position_j(int n);
+
+	// 着手可能な手のビットボード表現を返す
+	__declspec(dllexport) unsigned long long possible_moves(unsigned long long dark, unsigned long long light, int player);
 }
--- ReversiAiDllTest/unittest.cpp
+++ ReversiAiDllTest/unittest.cpp
@@ -13,20 +13,45 @@ namespace ReversiAiDllTest
 	TEST_CLASS(UnitTest)
 	{
 	public:
-		// 
-		TEST_METHOD(PositionEncoderTestMethod)
+		// 位置座標とビット番号の相互変換テスト
+		TEST_METHOD(PositionConversionTest)
 		{
-			// 
-
-			// 
-
-			// 
-			Assert::AreEqual(decode_position_i(encode_position(3, 5)), 3);
-			Assert::AreEqual(decode_position_j(encode_position(3, 5)), 5);
-			// 
+			// 位置座標からビット番号への変換テスト
+			Assert::AreEqual(position_to_index(0, 0), 0);
+			Assert::AreEqual(position_to_index(1, 0), 1);
+			Assert::AreEqual(position_to_index(7, 0), 7);
+			Assert::AreEqual(position_to_index(0, 1), 8);
+			Assert::AreEqual(position_to_index(4, 2), 20);
+			Assert::AreEqual(position_to_index(5, 3), 29);
+			Assert::AreEqual(position_to_index(6, 5), 46);
+			Assert::AreEqual(position_to_index(7, 7), 63);
+			// ビット番号からi座標を取り出すテスト
+			Assert::AreEqual(index_to_position_i(0), 0);
+			Assert::AreEqual(index_to_position_i(1), 1);
+			Assert::AreEqual(index_to_position_i(7), 7);
+			Assert::AreEqual(index_to_position_i(8), 0);
+			Assert::AreEqual(index_to_position_i(21), 5);
+			Assert::AreEqual(index_to_position_i(27), 3);
+			Assert::AreEqual(index_to_position_i(44), 4);
+			Assert::AreEqual(index_to_position_i(63), 7);
+			// ビット番号からj座標を取り出すテスト
+			Assert::AreEqual(index_to_position_j(0), 0);
+			Assert::AreEqual(index_to_position_j(7), 0);
+			Assert::AreEqual(index_to_position_j(8), 1);
+			Assert::AreEqual(index_to_position_j(16), 2);
+			Assert::AreEqual(index_to_position_j(30), 3);
+			Assert::AreEqual(index_to_position_j(39), 4);
+			Assert::AreEqual(index_to_position_j(55), 6);
+			Assert::AreEqual(index_to_position_j(63), 7);
 		}
 
-		//
+		// 着手可能な手を調べるテスト
+		TEST_METHOD(PossibleMoveTestMethod)
+		{
+
+		}
+
+		// ネガマックス法のテスト
 		TEST_METHOD(NegaMaxTestMethod)
 		{
 
