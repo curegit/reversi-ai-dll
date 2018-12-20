a8bbd17242119a38fc5257cd4823b7a80769f0c1
curegit <37978051+curegit@users.noreply.github.com>
2018-12-20 23:20:51 +0900

    盤表示関数を実装

--- ReversiConsole/ReversiConsole.cpp
+++ ReversiConsole/ReversiConsole.cpp
@@ -1,21 +1,30 @@
-﻿// ReversiConsole.cpp : このファイルには 'main' 関数が含まれています。プログラム実行の開始と終了がそこで行われます。
-//
-
-#include "pch.h"
+﻿#include "pch.h"
+#include "ReversiAiDll.h"
 #include <iostream>
+using namespace std;
 
-int main()
+// 
+void print_board(unsigned long long player1, unsigned long long player2)
 {
-    std::cout << "Hello World!\n"; 
+	cout << "  | a b c d e f g h\n";
+	cout << "-------------------\n";
+	for (int i = 1; i <= 8; i++)
+	{
+		cout << i << " |";
+		unsigned long long p1 = player1 >> 8 * (8 - i);
+		unsigned long long p2 = player2 >> 8 * (8 - i);
+		for (int j = 7; j >= 0; j--)
+		{
+			char c = ' ';
+			if (p1 >> j & 0x01ull) c = 'x';
+			if (p2 >> j & 0x01ull) c = 'o';
+			cout << " " << c;
+		}
+		cout << "\n";
+	}
 }
 
-// プログラムの実行: Ctrl + F5 または [デバッグ] > [デバッグなしで開始] メニュー
-// プログラムのデバッグ: F5 または [デバッグ] > [デバッグの開始] メニュー
-
-// 作業を開始するためのヒント: 
-//    1. ソリューション エクスプローラー ウィンドウを使用してファイルを追加/管理します 
-//   2. チーム エクスプローラー ウィンドウを使用してソース管理に接続します
-//   3. 出力ウィンドウを使用して、ビルド出力とその他のメッセージを表示します
-//   4. エラー一覧ウィンドウを使用してエラーを表示します
-//   5. [プロジェクト] > [新しい項目の追加] と移動して新しいコード ファイルを作成するか、[プロジェクト] > [既存の項目の追加] と移動して既存のコード ファイルをプロジェクトに追加します
-//   6. 後ほどこのプロジェクトを再び開く場合、[ファイル] > [開く] > [プロジェクト] と移動して .sln ファイルを選択します
+int main(const int argc, const char *argv[])
+{
+	print_board(3255235, 12444141);
+}
